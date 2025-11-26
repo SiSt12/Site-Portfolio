2c84b73d2e9e06a50814bbcef53c2f4efa2d6f48
Silvaldo Stolarz <59754097+SiSt12@users.noreply.github.com>
2025-11-25 22:21:25 -0300

    Implement URL rewrites for habit tracking
    Added rewrites for habit tracking URLs.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/habit',
+        destination: 'https://habit-tracker-clone-c2tg.vercel.app/',
+      },
+      {
+        source: '/habit/:path*',
+        destination: 'https://habit-tracker-clone-c2tg.vercel.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
